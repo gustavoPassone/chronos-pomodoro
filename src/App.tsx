f12ebb3dcd83d2dc46d802fb6a89604ed8a6f20a
gustavoPassone <gmpassone@gmail.com>
2025-09-23 10:47:53 -0300

    models TaskModel e TaskStateModel

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import { Home } from "./pages/Home";
 
 import "./styles/theme.css";
 import "./styles/global.css";
+import { AboutPomodoro } from "./pages/AboutPomodoro";
+import { NotFound } from "./pages/NotFound";
 
 export function App() {
-  return <Home />
+  return <NotFound />
 }
