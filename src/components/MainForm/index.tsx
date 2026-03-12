9aedf0f2b9933d816522ada1f9b9bafa62ad4aae
gustavoPassone <gmpassone@gmail.com>
2026-03-12 10:01:30 -0300

    Enviando o estado via props por vários componentes

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,30 +2,49 @@ import { PlayCircleIcon } from "lucide-react";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
+import type { HomeProps } from "../../pages/Home";
 
-export function MainForm() {
-  return (
-    <form className="form" action="">
-      <div className="formRow">
-        <DefaultInput
-          labelText="task"
-          id="meuInput"
-          type="text"
-          placeholder="Digite algo"
-        />
-      </div>
+export function MainForm({ state, setState }: HomeProps) {
+    function handleClick() {
+        setState((prevState) => {
+            return {
+                ...prevState,
+                config: {
+                    ...prevState.config,
+                    workTime: 34,
+                },
+                formattedSecondsRemaining: "23:34",
+            };
+        });
+    }
 
-      <div className="formRow">
-        <p>Lorem ipsum dolor sit amet.</p>
-      </div>
+    return (
+        <form className="form" action="">
+            <div>
+                <button type="button" onClick={handleClick}>
+                    Clicar
+                </button>
+            </div>
+            <div className="formRow">
+                <DefaultInput
+                    labelText="task"
+                    id="meuInput"
+                    type="text"
+                    placeholder="Digite algo"
+                />
+            </div>
 
-      <div className="formRow">
-        <Cycles />
-      </div>
+            <div className="formRow">
+                <p>Proximo intervalo é {state.config.workTime}min</p>
+            </div>
 
-      <div className="formRow">
-        <DefaultButton icon={<PlayCircleIcon />} />
-      </div>
-    </form>
-  );
+            <div className="formRow">
+                <Cycles />
+            </div>
+
+            <div className="formRow">
+                <DefaultButton icon={<PlayCircleIcon />} />
+            </div>
+        </form>
+    );
 }
